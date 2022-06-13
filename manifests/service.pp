8400938094c59004d96d45e5b120a540e14d57ab
root <root@e328a1fbcd1c.mylabserver.com>
2022-06-13 02:03:16 +0000

    added service class

---
+++ manifests/service.pp
@@ -0,0 +1,11 @@
+# @summary 
+# service class
+#
+class apache::service {
+  service { "${apache::service_name}":
+    alias      => 'apache_service',
+    ensure     => $apache::service_ensure,
+    enable     => $apache::service_enable,
+    hasrestart => true,
+  }
+}
